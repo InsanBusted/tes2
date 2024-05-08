936d1bd5b9b19fce350a716c3789936673f960d0
InsanBusted <insanbusted@gmail.com>
2024-05-08 14:21:25 +0700

    Update

---
+++ admin/included/footer.php
@@ -0,0 +1,20 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+
+  <script src="<?= $url ?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="<?= $url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE -->
+<script src="<?= $url ?>dist/js/adminlte.js"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="<?= $url ?>plugins/chart.js/Chart.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= $url ?>dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?= $url ?>dist/js/pages/dashboard3.js"></script>
